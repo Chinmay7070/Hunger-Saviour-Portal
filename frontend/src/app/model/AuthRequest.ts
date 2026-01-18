8088314980249cf3f18e4b7bbaceaa8838adc310
Chinmay Chaudhari <chaudharichinmay227@gmail.com>
2026-01-18 17:49:41 +0530

    Remove frontend src directory

--- frontend/src/app/model/AuthRequest.ts
+++
@@ -1,6 +0,0 @@
-export interface AuthRequst{
-
-    username:string,
-    password:string
-
-}
--- frontend/src/app/model/Restaurant.ts
+++
@@ -1,8 +0,0 @@
-export interface Restaurant{
-    restaurantId: number;
-    restaurantName: String;
-    rating:number;
-    menuTypes: string[];
-    imageUrl: string;
-    location: string;
-}
